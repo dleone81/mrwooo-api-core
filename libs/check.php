7572a6ab2efa78099ca1a6e081e2045e2903d10f
Domenico Leone <domenico.leone@gmail.com>
2018-05-30 02:16:54 +0200

    added check feature

---
+++ libs/check.php
@@ -0,0 +1,10 @@
+<?php
+    class MRWOOO_LIB_Check {
+		private static $event = 'check/check/';
+        
+        public static function hello(){
+            echo 'Hello, it works well!';
+        }
+    }
+
+?>
