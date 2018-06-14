5081adb4010b80649c427f46140abb6510097ff1
Domenico Leone <domenico.leone@gmail.com>
2018-06-14 02:05:59 +0200

    minor

--- libs/check.php
+++ libs/check.php
@@ -1,10 +1,9 @@
 <?php
-    class MRWOOO_LIB_Check {
+    class MRWOOO_API_LIBS_Check {
 		private static $event = 'check/check/';
         
         public static function hello(){
             echo 'Hello, it works well!';
         }
     }
-
 ?>
