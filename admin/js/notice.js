3d1fafa9f2bced2a35a8ecd9ec5b4e6c16b2f2f1
Domenico Leone <domenico.leone@gmail.com>
2018-06-14 02:08:28 +0200

    added ajax loader and notice

---
+++ admin/js/notice.js
@@ -0,0 +1,76 @@
+var notice = {
+    new: function(o) {
+        var _i = jQuery.extend(true, {}, this);
+        jQuery.extend(true, _i.options, o);
+        return _i;
+    },
+    init: function(){
+        ts = this;
+    },
+    getNotice: function(method, status, msg = null, data = null){
+
+        var notice = jQuery('div.notice');
+        if(notice.length > 0)
+            notice.remove();
+
+        // admin notice
+        var container = jQuery('<div>');
+        container.addClass('notice');
+
+        var p = jQuery('<p>');
+        container.append(p);
+
+        /* 
+        *   notice class wp legacy
+        *   notice-info (blue)
+        *   notice-success (green)
+        *   notice-warning (orange)
+        *   notice-error (red)
+        */
+
+        if(method == 'exportUsersData'){
+            switch(status){
+                case '201':
+                    var msg = 'File ready. Download now!';
+                    var a = jQuery('<a>');
+
+                    container.addClass('notice-info');
+                    a.html(msg);
+                    p.append(a);
+                    jQuery('div#wpbody-content').prepend(container);
+                    csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(data);
+                    jQuery(".notice a").attr({
+                        "href": csvData,
+                        "download": "export.csv"
+                    });
+                    break;
+                default:
+                    break;
+            }
+
+        } else if(method == 'importUsersData') {
+            switch(status){
+                case '202':
+                    container.addClass('notice-success');
+                    jQuery(p).html(msg);
+                    jQuery('div#wpbody-content').prepend(container);
+                    break;
+                case '400':
+                    container.addClass('notice-warning');
+                    jQuery(p).html(msg);
+                    jQuery('div#wpbody-content').prepend(container);
+                    break;
+                case '406':
+                    container.addClass('notice-error');
+                    jQuery(p).html(msg);
+                    jQuery('div#wpbody-content').prepend(container);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+jQuery(document).ready(function($){
+    notice.new({}).init();
+});
---
+++ libs/ajax.php
@@ -0,0 +1,24 @@
+<?php
+    class MRWOOO_API_LIBS_Ajax {
+        function loader(){ ?>
+            <script type="text/javascript">
+                jQuery(document).ready(function($){
+
+                    // create a loader div
+                    var loader = $('<div>');
+                    loader.addClass('mrwooo ajaxloader');
+                    $('body').prepend(loader);
+                    $('.mrwooo.ajaxloader').hide();
+
+                    $('.mrwooo').ajaxStart(function() {
+                        $('.mrwooo.ajaxloader').fadeIn(1000);
+                    });
+                    $('.mrwooo').ajaxStop(function() {
+                        $('.mrwooo.ajaxloader').fadeOut(500);
+                    });
+                    
+                });
+            </script>
+        <? }
+    }
+?>
